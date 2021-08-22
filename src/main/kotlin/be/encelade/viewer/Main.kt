802a5e974dac0eb470447305d9477e016aa67e38
benckx <benoit.vleminckx@restore.eu>
2021-08-22 18:23:28 +0200

    build scene

--- src/main/kotlin/be/encelade/viewer/Main.kt
+++ src/main/kotlin/be/encelade/viewer/Main.kt
@@ -1,6 +1,47 @@
 package be.encelade.viewer
 
+import be.encelade.chimp.material.MaterialDefinitions
+import be.encelade.chimp.utils.ColorHelperUtils.ColorRGBA
+import be.encelade.ouistiti.CameraManager
+import com.jme3.app.SimpleApplication
+import com.jme3.system.AppSettings
+
 fun main() {
     val frame = AssetFrame()
     frame.isVisible = true
+
+    val settings = AppSettings(true)
+    settings.title = "JME asset viewer"
+    settings.isFullscreen = false
+    settings.isVSync = false
+    settings.samples = 16
+    settings.setResolution(1280, 720)
+
+    val simpleApp = ViewerAppApp()
+    simpleApp.setSettings(settings)
+    simpleApp.isShowSettings = false
+    simpleApp.start()
+}
+
+class ViewerAppApp : SimpleApplication() {
+
+    private lateinit var cameraManager: CameraManager
+
+    override fun simpleInitApp() {
+        // init chimp-utils API for materials
+        MaterialDefinitions.load(assetManager)
+
+        // init CameraManager
+        cameraManager = CameraManager(this)
+        cameraManager.addDefaultKeyMappings()
+
+        // build scene
+        viewPort.backgroundColor = ColorRGBA("#1c3064")
+        rootNode.attachChild(SceneNode())
+    }
+
+    override fun simpleUpdate(tpf: Float) {
+        cameraManager.simpleUpdate(tpf)
+    }
+
 }
